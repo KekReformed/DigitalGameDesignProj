fd282a900784c950020d873a5bf27524f7aad9e8
Jack <exploretheinternet32@gmail.com>
2024-03-12 01:50:36 +0000

    Camera transitions working and editable

---
+++ Assets/Scripts/CameraChanger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChanger : MonoBehaviour
+{
+    public GameObject virtualCam;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            virtualCam.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            virtualCam.SetActive(false);
+        }
+    }
+
+    private void Update(){
+        return;
+    }
+}
